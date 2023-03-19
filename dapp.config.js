9a835966680ce1d6be29328e07aa76e5d6b858c1
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2023-03-19 15:36:48 -0400

    Update dapp.config.js

--- dapp.config.js
+++ dapp.config.js
@@ -4,7 +4,7 @@ const config = {
   contractAddress: '0x3aC22795304A27edb04Cfe2475DCEf0c5C8B5539',
   maxMintAmount: 1,
   presaleMaxMintAmount: 1,
-  price: (address === '0xa09b260809915dA08f831A53da431Aa3c1d03618') ? 0 : 20000
+  price: 20000
 }
 
 export { config }
