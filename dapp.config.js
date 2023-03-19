1841feafcd33ffce7bbcd998bad4131b79810528
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2023-03-19 15:55:23 -0400

    Update dapp.config.js

--- dapp.config.js
+++ dapp.config.js
@@ -6,34 +6,3 @@ const config = {
   presaleMaxMintAmount: 1,
   price: 20000
 }
-
-async function mint() {
-  const provider = await detectEthereumProvider();
-  if (provider) {
-    try {
-      const web3 = new Web3(provider);
-      const accounts = await web3.eth.requestAccounts();
-      const contract = new web3.eth.Contract(contractAbi, config.contractAddress);
-      const ownerAddress = '0xa09b260809915dA08f831A53da431Aa3c1d03618';
-      const mintAmount = config.maxMintAmount;
-      const account = accounts[0];
-      let weiValue = config.price * mintAmount;
-      if (account === ownerAddress) {
-        weiValue = 0;
-      }
-      const gasPrice = await web3.eth.getGasPrice();
-      const gasLimit = 300000;
-      const result = await contract.methods.mint(account, mintAmount).send({
-        from: account,
-        value: weiValue,
-        gasPrice: gasPrice,
-        gasLimit: gasLimit
-      });
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    }
-  } else {
-    console.error('Please install MetaMask!');
-  }
-}
