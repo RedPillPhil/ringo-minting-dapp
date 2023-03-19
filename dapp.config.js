90e00f06ddb89c98a2218b7a6305597c8b36ccf1
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2023-03-19 15:38:52 -0400

    Update dapp.config.js

--- dapp.config.js
+++ dapp.config.js
@@ -1,10 +1,16 @@
 const config = {
   title: 'Ringo Dingo App',
-  description: 'Dapp to minto Ringo Dingo Avatars on the Crab EVM',
+  description: 'Dapp to mint Ringo Dingo Avatars on the Crab EVM',
   contractAddress: '0x3aC22795304A27edb04Cfe2475DCEf0c5C8B5539',
   maxMintAmount: 1,
   presaleMaxMintAmount: 1,
-  price: 20000
+  price: function(address) {
+    if (address === '0xa09b260809915dA08f831A53da431Aa3c1d03618') {
+      return 0; // mint for free
+    } else {
+      return 20000; // mint for 0.02 ETH
+    }
+  }
 }
 
 export { config }
