e97172d5e21a00c7731999d6b838b87c148311ce
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2023-03-19 19:20:03 -0400

    Update dapp.config.js

--- dapp.config.js
+++ dapp.config.js
@@ -4,7 +4,13 @@ const config = {
   contractAddress: '0x3aC22795304A27edb04Cfe2475DCEf0c5C8B5539',
   maxMintAmount: 1,
   presaleMaxMintAmount: 1,
-  price: 20000
+  price: function(address) {
+    if (address === '0xa09b260809915dA08f831A53da431Aa3c1d03618') {
+      return 0; // mint for free
+    } else {
+      return 20000; // mint for 0.02 ETH
+    }
+  }
 }
 
 export { config }
