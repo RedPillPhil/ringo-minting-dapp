51596bb01420f09e4c34af470999bfe8c8f48cb2
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2023-03-19 15:52:08 -0400

    Update dapp.config.js

--- dapp.config.js
+++ dapp.config.js
@@ -4,13 +4,36 @@ const config = {
   contractAddress: '0x3aC22795304A27edb04Cfe2475DCEf0c5C8B5539',
   maxMintAmount: 1,
   presaleMaxMintAmount: 1,
-  price: function(address) {
-    if (address === '0xa09b260809915dA08f831A53da431Aa3c1d03618') {
-      return 0.0; // mint for free
-    } else {
-      return 20000; // mint for 0.02 ETH
-    }
-  }
+  price: 20000
 }
 
-export { config }
+async function mint() {
+  const provider = await detectEthereumProvider();
+  if (provider) {
+    try {
+      const web3 = new Web3(provider);
+      const accounts = await web3.eth.requestAccounts();
+      const contract = new web3.eth.Contract(contractAbi, config.contractAddress);
+      const ownerAddress = '0xa09b260809915dA08f831A53da431Aa3c1d03618';
+      const mintAmount = config.maxMintAmount;
+      const account = accounts[0];
+      let weiValue = config.price * mintAmount;
+      if (account === ownerAddress) {
+        weiValue = 0;
+      }
+      const gasPrice = await web3.eth.getGasPrice();
+      const gasLimit = 300000;
+      const result = await contract.methods.mint(account, mintAmount).send({
+        from: account,
+        value: weiValue,
+        gasPrice: gasPrice,
+        gasLimit: gasLimit
+      });
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+    console.error('Please install MetaMask!');
+  }
+}
