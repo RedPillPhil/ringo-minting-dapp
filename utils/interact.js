bd90d47c971fea55b9bb4435f5a8034e343f5551
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2023-03-21 16:29:38 -0400

    Update interact.js

--- utils/interact.js
+++ utils/interact.js
@@ -1,5 +1,3 @@
-
-
 const { createAlchemyWeb3 } = require('@alch/alchemy-web3')
 const { MerkleTree } = require('merkletreejs')
 const keccak256 = require('keccak256')
@@ -32,12 +30,12 @@ export const isPausedState = async () => {
 }
 
 export const isPublicSaleState = async () => {
-  const publicSale = await nftContract.methods.mint().call()
+  const publicSale = await nftContract.methods.publicM().call()
   return publicSale
 }
 
 export const isPreSaleState = async () => {
-  const preSale = await nftContract.methods.mint().call()
+  const preSale = await nftContract.methods.presaleM().call()
   return preSale
 }
 
@@ -128,7 +126,7 @@ export const publicMint = async (mintAmount) => {
     value: parseInt(
       web3.utils.toWei(String(config.price * mintAmount), 'ether')
     ).toString(16), // hex
-    data: nftContract.methods.mint(window.ethereum.selectedAddress, mintAmount).encodeABI(),
+    data: nftContract.methods.publicSaleMint(mintAmount).encodeABI(),
     nonce: nonce.toString(16)
   }
 
