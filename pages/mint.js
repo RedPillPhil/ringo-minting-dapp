a820319e41719762f9208b61a8c47e764f19ee74
RedPillPhil <47307224+RedPillPhil@users.noreply.github.com>
2023-03-21 01:18:25 -0400

    Update mint.js

--- pages/mint.js
+++ pages/mint.js
@@ -111,7 +111,7 @@ export default function Mint() {
   const publicMintHandler = async () => {
     setIsMinting(true)
 
-    const { success, status } = await publicMint(mintAmount)
+    const { success, status } = await publicMint(account, mintAmount)
 
     setStatus({
       success,
